a7855747c0b031e6f94f84419c0921b6f21ce0c5
taro-hida <hidataro11@gmail.com>
2020-01-10 11:22:08 +0900

    first commit
    動作確認済み。テストコード付き

---
+++ Gojuonbase.php
@@ -0,0 +1,65 @@
+<?php
+
+Class Gojuonbase
+{
+    protected $gojuon = array("あ","い","う","え","お","か","き","く","け","こ","さ","し","す","せ","そ","た","ち","つ","て","と","な","に","ぬ","ね","の","は","ひ","ふ","へ","ほ","ま","み","む","め","も","や","ゆ","よ","ら","り","る","れ","ろ","わ","ゐ","ゑ","を","ん");
+    protected $characters_counts = 48;
+
+    protected $decimal;
+
+    public function __construct($input)
+    {
+        if(is_string($input))
+        {
+            $this->decimal = $this->__load_hiragana($input);
+        }
+        elseif(is_int($input))
+        {
+           $this->decimal = $input;
+        }
+        else
+        {
+            return False;
+            // Todo: cause error.
+        }
+    } 
+
+    protected function __load_hiragana($input)
+    {
+        // str_splitはマルチバイト非対応のため使用できなかった。
+        // こちらから借用：http://scrap.php.xdomain.jp/php_str_split/
+        // mb_str_splitがphpで採用されたら乗り換えよう
+        // https://wiki.php.net/rfc/mb_str_split
+        $multibyte_array = preg_split("//u" , $input, -1, PREG_SPLIT_NO_EMPTY);
+        $reversed_input = array_reverse($multibyte_array, false); 
+
+        $sum_decimal = 0;
+        foreach($reversed_input as $seq => $word)
+        {
+            $match_word_seq = array_search($word, $this->gojuon);
+            // match_word_seqかmatch_word_numか迷う
+            if(!$match_word_seq){
+                return False;
+                // Todo: cause error.
+            }
+            $sum_decimal = $sum_decimal + (int)$match_word_seq * ((int)$this->characters_counts ** (int)$seq);
+        }
+
+        return $sum_decimal;
+    }
+
+    public function decimal()
+    {
+        return $this->decimal;
+    }
+}
+
+function gojuonToDecimal($input)
+{
+    $obj = new Gojuonbase($input);
+    return $obj->decimal();
+}
+
+        
+$text = "なのやつ";
+print_r(gojuonToDecimal($text));
